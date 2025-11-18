f7a27a605ae045dfe50018f9384fa5699ed71552
ANA PAULA WANDERLEY <54805005890@imaccommand024.local>
2025-11-18 11:53:56 -0300

    exercicio02

---
+++ exercicio02.js
@@ -0,0 +1,33 @@
+let numero = window.prompt("Digite um número de 1 á 7")
+switch (numero) {
+    case "1":
+        console.log("Segunda-Feira")
+
+
+        break;
+    case "2":
+        console.log("Terça-Feira")
+
+        break;
+    case "3":
+        console.log("Quarta-Feira")
+
+        break;
+    case "4":
+        console.log("Quinta-feira")
+
+        break;
+    case "5":
+        console.log("Sexta-Feira")
+
+        break;
+    case "6":
+        console.log("Sábado")
+
+        break;
+    case "7":
+        console.log("Domingo")
+    default:
+        console.log("Dia inexistente")
+        break;
+}
